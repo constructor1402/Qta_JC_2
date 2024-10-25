96080623b2667e06674cf33534b2100755f627e4
constructor1402 <cristianestudiante15@gmail.com>
2024-10-24 19:03:48 -0500

    inicio de sesion

---
+++ app/src/main/java/com/example/qta_jc_2/inciodesesion.kt
@@ -0,0 +1,20 @@
+package com.example.qta_jc_2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class inciodesesion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_inciodesesion)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
